648d28b64251f998bda98a507badafd52c45c50b
Nabin Lama <lamanabin6936@gmail.com>
2024-09-03 16:00:10 +0545

    routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,55 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/user/register", (req, res) => {
+const userInfoValidation = (action) => {
+  switch (action) {
+    case "register":
+      return [
+        body("firstName", "Please enter First Name")
+          .exists()
+          .isLength({ min: 1 }),
+        body("lastName", "Please enter Last Name")
+          .exists()
+          .isLength({ min: 1 }),
+        body("email", "Invalid email").exists().isEmail(),
+        body(
+          "password",
+          "Password must be at least 8 characters and include a mix of letters, numbers, and special characters"
+        )
+          .exists()
+          .isLength({ min: 8 })
+          .matches(
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+            "i"
+          ),
+        body(
+          "confirmPassword",
+          "Confirm password must be at least 8 characters"
+        )
+          .exists()
+          .isLength({ min: 8 })
+          .custom((confirmPassword, { req }) => {
+            if (confirmPassword !== req.body.password) {
+              throw new Error("Password does not match");
+            }
+            return true;
+          }),
+      ];
+
+    default:
+      break;
+  }
+};
+
+router.post("/register", userInfoValidation("register"), (req, res) => {
   const user = { name: "Nabin" };
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(401).send(errors.array());
+  }
   res.send(user);
 });
 
