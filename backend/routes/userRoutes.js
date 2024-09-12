ce53eb14edb0e9821e9644d644ca5c7439e3f47c
Nabin Lama <lamanabin6936@gmail.com>
2024-09-12 14:25:29 +0545

    jwt auth in utils

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { body, validationResult } from "express-validator";
 import bcrypt from "bcrypt";
 import { pool } from "../db/dbConnect";
+import { getToken } from "../util";
 
 const router = express.Router();
 
@@ -89,7 +90,7 @@ router.post("/register", userInfoValidation("register"), async (req, res) => {
 
   const newUser = queryRes.rows[0];
 
-  res.send(newUser);
+  res.send({ ...newUser, token: getToken(newUser) });
 });
 
 router.post("/signin", userInfoValidation("signin"), async (req, res) => {
@@ -127,7 +128,7 @@ router.post("/signin", userInfoValidation("signin"), async (req, res) => {
 
   const { passwordhash, ...userWithoutPw } = possibleUser;
 
-  res.send(userWithoutPw);
+  res.send({ ...userWithoutPw, token: getToken(userWithoutPw) });
 });
 
 export default router;
